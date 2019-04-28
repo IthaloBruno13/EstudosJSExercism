85db891945920a60d5f6d896e7f7e25c8bf9d5fc
Ithalo <ithalo.bruno85@gmail.com>
2019-04-28 16:05:25 -0300

    Melhorando o codigo

--- twoFer/twoFer.js
+++ twoFer/twoFer.js
@@ -1,3 +1,1 @@
-export const twoFer = (name) =>{
-    return name !== undefined ? `One for ${name}, one for me` : "One for you, one for me";
-}
+export const twoFer = (name = 'you') => `One for ${name}, one for me`;
